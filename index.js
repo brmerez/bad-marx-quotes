491f1aa353e68c6aabb65dc8930a32ea6c08e66c
brmerez <brmerez@gmail.com>
2022-01-14 11:24:48 -0300

    added some endpoints

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ function sendTweet(){
   console.log(`Sent tweet:\t${frase}`);
 }
 
+app.get("/", (req, res)=>{
+  res.send(`<h1>${getFrase}</h1>`);
+})
+
+app.get("/mandalavai", (req, res)=>{
+  sendTweet();
+  res.send(`<h1>${getFrase}</h1>`);
+});
+
 setInterval(sendTweet, INTERVALO);
 
 app.listen(PORT, ()=> console.log("Listening on " + PORT));
