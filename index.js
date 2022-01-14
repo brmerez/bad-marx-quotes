b6f3f15a77301d869a405d89a24b4c057e169a02
Bruno Merez <69603474+brmerez@users.noreply.github.com>
2022-01-13 22:13:02 -0300

    Aumentando intervalo pra 2 horas
    ta maluco kkk

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const getFrase = require("./src/phrase");
 const { client, tweet } = require("./src/twitter");
-const INTERVALO = 10000 // meia hora 1800000
+const INTERVALO = 7200000 // meia hora 1800000; 2 horas 7200000
 
-// Manda um tweet a cada 30 minutos
+// Manda um tweet a cada 2 horas
 async function Main() {
     while (true) {
       await new Promise(resolve => setTimeout(resolve, INTERVALO));
