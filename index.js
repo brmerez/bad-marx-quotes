24146b3057bbefb7bd71c368a782ff36f7cacf50
brmerez <brmerez@gmail.com>
2022-01-14 00:33:55 -0300

    more templates

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 require("dotenv").config();
 const getFrase = require("./src/phrase");
 const { client, tweet } = require("./src/twitter");
-const INTERVALO = 10000 // meia hora 1800000
+// const INTERVALO = 5000 // meia hora 1800000
 
 // Manda um tweet a cada 30 minutos
-async function Main() {
-    while (true) {
-      await new Promise(resolve => setTimeout(resolve, INTERVALO));
-      const frase = getFrase()
-      console.log(`Mandando Tweet:\t${frase}`)
-      tweet(frase)
-      console.log("Tweet enviado!\n")
-    }
-}
+// async function Main() {
+//     while (true) {
+//       await new Promise(resolve => setTimeout(resolve, INTERVALO));
+//       const frase = getFrase()
+//       console.log(`Mandando Tweet:\t${frase}`)
+//       tweet(frase)
+//       console.log("Tweet enviado!\n")
+//     }
+// }
 
-Main();
+// Main();
+
+const frase = getFrase()
+console.log(`Mandando Tweet:\t${frase}`)
+tweet(frase)
+console.log("Tweet enviado!\n")
