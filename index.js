ce70d7bb101f2d5a5ea212b2f4c341e104c78b3a
Bruno Merez <69603474+brmerez@users.noreply.github.com>
2022-01-14 10:57:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
 const getFrase = require("./src/phrase");
 const { client, tweet } = require("./src/twitter");
-<<<<<<< HEAD
-// const INTERVALO = 5000 // meia hora 1800000
+
+const INTERVALO = 1800000 // meia hora 1800000
 
 // Manda um tweet a cada 30 minutos
-// async function Main() {
-//     while (true) {
-//       await new Promise(resolve => setTimeout(resolve, INTERVALO));
-//       const frase = getFrase()
-//       console.log(`Mandando Tweet:\t${frase}`)
-//       tweet(frase)
-//       console.log("Tweet enviado!\n")
-//     }
-// }
+async function Main() {
+    while (true) {
+      await new Promise(resolve => setTimeout(resolve, INTERVALO));
+      const frase = getFrase()
+      console.log(`Mandando Tweet:\t${frase}`)
+      tweet(frase)
+      console.log("Tweet enviado!\n")
+    }
+}
 
-// Main();
+Main();
 
-const frase = getFrase()
-console.log(`Mandando Tweet:\t${frase}`)
-tweet(frase)
-console.log("Tweet enviado!\n")
+//const frase = getFrase()
+//console.log(`Mandando Tweet:\t${frase}`)
+//tweet(frase)
+//console.log("Tweet enviado!\n")
